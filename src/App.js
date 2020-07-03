457a3c33413d95daa3b74a8c6d5f3134a7289c3c
priyansh-garg <2000priyansh@gmail.com>
2020-07-03 06:57:25 +0530

    Add Header

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import './App.css';
 import "./pages/homepage/homepage.styles.scss";
 import Homepage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 function App() {
   return (
     <div>
+    
     <BrowserRouter>
+    <Header />
       <Switch>
       <Route exact path="/" component={Homepage}/>
       
       <Route exact path="/shop" component={Shop}/>
       </Switch>
       </BrowserRouter>
+      
     </div>
   );
 }
