07d9bb3f6b7d509bcf099eafa6ff67dd0614b28e
priyansh-garg <2000priyansh@gmail.com>
2020-07-07 03:33:56 +0530

    Add Local storage using redux-persist

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
-import store from './redux/store';
+
+import {persistor,store} from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 ReactDOM.render(
   <React.StrictMode>
   <Provider store={store}>
+  <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
     </Provider>
   </React.StrictMode>
   ,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,13 +1,21 @@
 
 import userReducer from "./user/user.reducer";
 
+import storage from "redux-persist/lib/storage";
 import {combineReducers} from "redux";
 import cartReducer from "./toggle-cart/toggle-cart.reducer";
+import { persistReducer } from "redux-persist";
 
 
+const persistConfig={
+    key:'root',
+    storage,
+    whitelist:['cart']
+};
 
-
-export default combineReducers({
+const rootReducer= combineReducers({
    user : userReducer, 
    cart: cartReducer
 });
+
+export default persistReducer(persistConfig,rootReducer);
