df63082a9741b9dbed0b9d3689618ed52280d700
priyansh-garg <2000priyansh@gmail.com>
2020-07-08 02:57:32 +0530

    Remove Contact Dummy Page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,9 +18,7 @@ const Header=({user,hidden})=>(
             <Link className="option" to="/shop">
                 SHOP
             </Link>
-            <Link className="option" to="/shop">
-                CONTACT
-            </Link>
+            
             {
                 user?
                 <div className="option" onClick={()=>auth.signOut()} >SIGN OUT</div>
