a556e7a1d5b6f669e6fa059b5825ce59d5532be0
priyansh-garg <2000priyansh@gmail.com>
2020-07-18 06:30:40 +0530

    Revert "Stripe Backend Done"
    This reverts commit e471442b5101f327be8afd7ba47c2fae21436589.

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,30 +1,15 @@
 import React from "react";
-import axios  from "axios";
+
 import StripeCheckout from "react-stripe-checkout";
 const StripeCheckoutButton=({price})=>{
-    const priceForStripe= price*100;
+    const priceForStripe= price*100/75;
     const publishableKey="pk_test_51H2MU9GEoeoKClkgZgiZ6K02SrFMvGD0GFH2JjGf9tEcXiJov2lQrq9g4MtW6VowpojZuNpRR421e2F7PqBwDxj700viYfgZxV";
 
-    const onToken = token => {
-        axios({
-          url: 'payment',
-          method: 'post',
-          data: {
-            amount: priceForStripe,
-            token: token
-          }
-        })
-          .then(response => {
-            alert('succesful payment');
-          })
-          .catch(error => {
-            console.log('Payment Error: ', error);
-            alert(
-              'There was an issue with your payment! Please make sure you use the provided credit card.'
-            );
-          });
-      };
-      return (
+    const onToken=token=>{
+        console.log(token);
+        alert("Payment Successful");
+    }
+    return (
         <StripeCheckout
         label="Pay Now"
         name="Ethnic Tradition"
@@ -33,7 +18,6 @@ const StripeCheckoutButton=({price})=>{
         image="https://svgshare.com/i/CUz.svg"
         description={`Your total amount is ${price}`}
         amount={priceForStripe}
-        currency="inr"
         panelLabel="Pay Now"
         token={onToken}
          stripeKey={publishableKey}
